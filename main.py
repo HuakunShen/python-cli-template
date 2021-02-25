abd3cf34c4ea7869824b7b766eec642ef4f4ee35
Huakun Shen <shenhukun@outlook.com>
2021-02-25 11:01:38 -0500

    Make --workspace a required argument

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ def main():
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(PROJECT_NAME)
-    parser.add_argument('-w', '--workspace', help='Workspace Path')
+    parser.add_argument('-w', '--workspace', required=True, help='Workspace Path')
     subparsers = parser.add_subparsers(help='sub-command help', dest='command')
     gen_config_parser = subparsers.add_parser('gen-config', help='Generate Configuration')
     run_parser = subparsers.add_parser('run', help='run help')
